74e1d1d93aed5190cd71bae56277bf902c0e284f
vasfidogan <vasfi.dogan@gmail.com>
2024-08-08 22:39:26 +0300

    made some conf changes

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,12 @@
+import logging
 from flask import Flask
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
 def hello():
+    app.logger.info('main request successfull')
     return "Hey, selam yabancı. Eskiden buralar dutluktu!"
 
 if __name__ == "__main__":
